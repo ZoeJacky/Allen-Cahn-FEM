d3a585509b11199d8feba543a35e1831b82f3889
李双双 <lishuangshuang00@gmail.com>
2024-10-02 11:05:41 +0200

    Initial Commit

---
+++ CMakeFiles/step-26.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/lishuangshuang/Desktop/Thesis/dealii-9.5.2/examples/step-26/step-26.cc" "CMakeFiles/step-26.dir/step-26.cc.o" "gcc" "CMakeFiles/step-26.dir/step-26.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/step-26.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/step-26.dir/step-26.cc.o"
+  "CMakeFiles/step-26.dir/step-26.cc.o.d"
+  "step-26"
+  "step-26.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/step-26.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
